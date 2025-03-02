427efc93f91e4b43536c41ee4042429a506fdacd
BiditG <biditgiri123@gmail.com>
2025-03-02 07:50:21 +0545

    Histogram

--- titanic_analysis.py
+++ titanic_analysis.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import seaborn as sn
+import matplotlib.pyplot as plt
+
 df = pd.read_csv("titanic.csv")
 df.head()
 df.describe()
-print (df.size)
+print(df.size)
 print(df.shape)
 print(df["age"].mean())
 print(df["age"].median())
@@ -11,6 +13,7 @@ print(df["age"].mode())
 print(df["age"].std())
 print(df["age"].var())
 print(df.columns)
-print(df["embarked"].unique())
 print(df["fare"].skew())
-print(df["fare"].hist())
+df["fare"].hist()
+plt.show()  
+
