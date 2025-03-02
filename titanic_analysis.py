2ce72684ea08823fbff80febf2a16539ce1f38dc
BiditG <biditgiri123@gmail.com>
2025-03-02 07:45:00 +0545

    Practice

---
+++ titanic_analysis.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sn
+df = pd.read_csv("titanic.csv")
+df.head()
+df.describe()
+print (df.size)
+print(df.shape)
+print(df["age"].mean())
+print(df["age"].median())
+print(df["age"].mode())
+print(df["age"].std())
+print(df["age"].var())
+print(df.columns)
+print(df["embarked"].unique())
+print(df["fare"].skew())
+print(df["fare"].hist())
